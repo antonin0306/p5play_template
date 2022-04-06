79cb87bc3fc08a426c828f75d7b88e90e739e78b
antonin0306 <84185944+antonin0306@users.noreply.github.com>
2022-04-06 16:58:56 -0300

    :) = quadrado. :) ->|<- (:.
    Contexto: Fiz o quadrado mexer de um lado para o outro. 👌😎

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
+var box;
 
 function setup() {
   createCanvas(400,400);
+  box = createSprite (200,200, 25,25);
 }
 
 function draw() 
 {
   background(30);
-
+  if (keyIsDown(RIGHT_ARROW)) {
+  box.x += 3;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+  box.x += -3;
+  }
+  drawSprites ();
 }
 
 
